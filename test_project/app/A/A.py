9cf5188bb73022c5302edbdaf02da3f4b5bc27b2
AlphaKR93 <dev@alpha93.kr>
2025-11-24 10:13:08 +0900

    chore: Removed obfuscated test source

--- test_project/app/A/A.py
+++
@@ -1,6 +0,0 @@
-from D import Æ
-from.B import C
-D=Æ(version='0')
-D.include_router(C)
-@D.get('')
-async def A():return{'version':'0'}
--- test_project/app/A/B.py
+++
@@ -1,5 +0,0 @@
-from D import Õ
-from A import D
-C=Õ(prefix='/router')
-@C.get('')
-async def A():return{'message':await D()}
